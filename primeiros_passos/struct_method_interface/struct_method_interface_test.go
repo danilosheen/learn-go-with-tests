642be6d52e115443226646dbc84c4d3b13def9f8
danilosheen <c.danilo.f.silva@gmail.com>
2023-04-10 16:34:06 -0300

    refact: implementação de interface e método Ve. A.

--- primeiros_passos/struct_method_interface/struct_method_interface_test.go
+++ primeiros_passos/struct_method_interface/struct_method_interface_test.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 )
 
+type Forma interface {
+	Area() float64
+}
+
 // declarando uma struct
 type Retangulo struct {
 	Largura float64
@@ -35,24 +39,24 @@ func TestPerimetro(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	t.Run("retangulos", func(t *testing.T) {
-		retangulo := Retangulo{10.0, 10.0}
-		resultado := retangulo.Area()
-		esperado := 100.0
+	verificaArea := func(t *testing.T, forma Forma, esperado float64) {
+        t.Helper()
+        resultado := forma.Area()
 
-		if resultado != esperado {
-			t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
-		}
-	})
-	t.Run("circulos", func(t *testing.T) {
-		circulo := Circulo{10.0}
-		resultado := circulo.Area()
-		esperado := 314.1592653589793
+        if resultado != esperado {
+            t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
+        }
+    }
 
-		if resultado != esperado {
-			t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
-		}
-	})
+	t.Run("retângulos", func(t *testing.T) {
+        retangulo := Retangulo{12.0, 6.0}
+        verificaArea(t, retangulo, 72.0)
+    })
+
+    t.Run("círculos", func(t *testing.T) {
+        circulo := Circulo{10}
+        verificaArea(t, circulo, 314.1592653589793)
+    })
 
 }
 
