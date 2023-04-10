8d4378d99bf46636e004c7b6e768d42f1e7ea1d8
danilosheen <c.danilo.f.silva@gmail.com>
2023-04-10 16:41:32 -0300

    refact: melhora na saída do test

--- primeiros_passos/struct_method_interface/struct_method_interface_test.go
+++ primeiros_passos/struct_method_interface/struct_method_interface_test.go
@@ -27,6 +27,15 @@ func (c Circulo) Area() float64 {
 	return math.Pi * c.Raio * c.Raio
 }
 
+type Triangulo struct {
+	Base   float64
+	Altura float64
+}
+
+func (t Triangulo) Area() float64 {
+	return (t.Base * t.Altura) * 0.5
+}
+
 func TestPerimetro(t *testing.T) {
 	retangulo := Retangulo{10.0, 10.0}
 	resultado := Perimetro(retangulo)
@@ -38,26 +47,24 @@ func TestPerimetro(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	testesArea := []struct {
+		nome    string
+		forma   Forma
+		temArea float64
+	}{
+		{nome: "Retângulo", forma: Retangulo{Largura: 12, Altura: 6}, temArea: 72.0},
+		{nome: "Círculo", forma: Circulo{Raio: 10}, temArea: 314.1592653589793},
+		{nome: "Triângulo", forma: Triangulo{Base: 12, Altura: 6}, temArea: 36.0},
+	}
 
-	verificaArea := func(t *testing.T, forma Forma, esperado float64) {
-        t.Helper()
-        resultado := forma.Area()
-
-        if resultado != esperado {
-            t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
-        }
-    }
-
-	t.Run("retângulos", func(t *testing.T) {
-        retangulo := Retangulo{12.0, 6.0}
-        verificaArea(t, retangulo, 72.0)
-    })
-
-    t.Run("círculos", func(t *testing.T) {
-        circulo := Circulo{10}
-        verificaArea(t, circulo, 314.1592653589793)
-    })
-
+	for _, tt := range testesArea {
+		t.Run(tt.nome, func(t *testing.T) {
+			resultado := tt.forma.Area()
+			if resultado != tt.temArea {
+				t.Errorf("%#v resultado %.2f, esperado %.2f", tt.forma, resultado, tt.temArea)
+			}
+		})
+	}
 }
 
 func Perimetro(retangulo Retangulo) float64 {
