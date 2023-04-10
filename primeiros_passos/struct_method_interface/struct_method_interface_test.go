3b89c62ae3828aa10ddc65d78e286772dc930eff
danilosheen <c.danilo.f.silva@gmail.com>
2023-04-10 16:05:11 -0300

    add: teste de função de perímetro e área simples

---
+++ primeiros_passos/struct_method_interface/struct_method_interface_test.go
@@ -0,0 +1,29 @@
+package structmethodinterface
+
+import "testing"
+
+func TestPerimetro(t *testing.T) {
+	resultado := Perimetro(10.0, 10.0)
+	esperado := 40.0
+
+	if resultado != esperado {
+		t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
+	}
+}
+
+func TestArea(t *testing.T){
+	resultado := Area(10.0, 10.0)
+	esperado := 100.0
+	
+	if resultado != esperado {
+		t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
+	}
+}
+
+func Perimetro(largura, altura float64) float64{
+	return 2 * (largura + altura)
+}
+
+func Area(altura, largura float64) float64 {
+	return altura * largura
+}
